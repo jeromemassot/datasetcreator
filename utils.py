fa0bdff2b8618e1a96af542b21eb607e17ed4495
Jerome Massot <jerome.massot.78@gmail.com>
2023-05-01 07:43:37 -0700

    Add BigQuery init from csv file function

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 from tensorflow.keras.preprocessing import image
 import tensorflow as tf
+
+from google.cloud import bigquery
+from google.cloud import storage
+import uuid
+import re
+
 from io import BytesIO
 from PIL import Image
 import numpy as np
@@ -217,3 +223,95 @@ def crop_image(image:Image, coords:list, w:int, h:int, expansion:float=1.) -> Im
         (int(coords[2]*w), int((1-coords[1])*h), int(coords[0]*w), int((1-coords[3])*h))
     )
     return image_crop
+
+
+def clean_caption(text:str) -> str:
+    rule1 = r'[Ff]ig(?:ure)?\s*(?:[,-:.])?\s*\d*(?:[,-:.])?\s*'
+    rule2 = r'[Tt]ab(?:le)?\s*(?:[,-:.])?\s*\d*(?:[,-:.])?\s*'
+    text = re.sub(rule1, '', text)
+    text = re.sub(rule2, '', text)
+    return text
+
+
+def init_bq_table_from_csv(bucket_name:str)-> None:
+    """
+    Initialize BigQuery table from csv file
+    :param bucket_name: name of the bucket
+    :return: None
+    """
+
+    # first the index needed to be modified to be used as unique identifier in BigQuery table
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob('figs_captions.csv')
+    bq_blob = bucket.blob('figs_captions_bq.csv')
+
+    with blob.open("r") as f:
+        modified_str = ''
+        for i, line in enumerate(f.readlines()):
+            if i==0:
+                modified_str += 'id|url|category|caption|tags|origin|page_index|status|backup\n'
+            else:
+                components = line.split('|')
+                figure_id = str(uuid.uuid5(uuid.NAMESPACE_X500 , components[9]))
+                figure_url = components[9].replace('\n', '')
+                figure_category = 'None'
+
+                figure_caption = components[8]
+                figure_caption = clean_caption(figure_caption)
+
+                figure_tags = 'None'
+                figure_status = 'Not Validated'
+                figure_backup = 'None'
+
+                origin = bucket_name
+                page_index = components[10]
+
+                bq_schema_data = [
+                    figure_id, figure_url, figure_category, figure_caption, 
+                    figure_tags, origin, page_index, figure_status, figure_backup
+                ]
+
+                new_line = "|".join(bq_schema_data)
+                modified_str += new_line + '\n'
+
+    with bq_blob.open("w") as f:
+        f.write(modified_str)
+
+    # construct a BigQuery client object.
+    bq_client = bigquery.Client()
+
+    # set table_id to the ID of the table to create.
+    table_id = "petroglyphs-nlp.geosciences_ai_datasets.geosciences-captioned-figures"
+
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("id", "STRING", "REQUIRED"),
+            bigquery.SchemaField("url", "STRING", "REQUIRED"),
+            bigquery.SchemaField("category", "STRING", "NULLABLE"),
+            bigquery.SchemaField("caption", "STRING", "NULLABLE"),
+            bigquery.SchemaField("tags", "STRING", "NULLABLE"),
+            bigquery.SchemaField("origin", "STRING", "REQUIRED"),
+            bigquery.SchemaField("page_index", "STRING", "REQUIRED"),
+            bigquery.SchemaField("status", "STRING", "NULLABLE"), 
+            bigquery.SchemaField("backup_url", "STRING", "NULLABLE")
+        ],
+        skip_leading_rows=1,
+        field_delimiter="|",
+        source_format=bigquery.SourceFormat.CSV,
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+    )
+
+    # create the bigquery job
+    modified_uri = f"gs:///{bucket_name}/figs_captions_bq.csv"
+
+    load_job = bq_client.load_table_from_uri(
+        modified_uri, table_id, job_config=job_config
+    )
+
+    # waits for the job to complete
+    load_job.result()
+
+    # make an API request
+    destination_table = bq_client.get_table(table_id)
+    print("Loaded {} rows.".format(destination_table.num_rows))
