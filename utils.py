311edb37a629959b50c3342ad07f418c17b615c5
Jerome Massot <jerome.massot.78@gmail.com>
2023-10-24 13:07:20 -0700

    Adding basin analysis in the topics list

--- utils.py
+++ utils.py
@@ -146,7 +146,7 @@ index2label = {k: v for k, v in enumerate(label2desc.keys())}
 
 # Tags
 tags_list = [
-    'chemistry', 'mineral', 'physics', 'thermo-dynamism', 'experiment', 'mining',
+    'basin analysis', 'chemistry', 'mineral', 'physics', 'thermo-dynamism', 'experiment', 'mining',
     'geophysics', 'geology', 'geomechanics', 'petrophysics', 'geochemistry',
     'structural', 'stratigraphy', 'sedimentology', 
     'reservoir', 'fluids', 'production', 'well', 'drilling', 'completion'
